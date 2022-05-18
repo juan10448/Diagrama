8f2f76e80c6fc7060994d3f812c443581bc4fb67
unknown <juan10448>
2022-05-18 15:47:33 -0500

    comienzo micelania

---
+++ Diagrama/SubClaseTerrestre.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagrama
+{
+    internal class SubClaseTerrestre
+    {
+        private string NombreTerrestre;
+        public int NumeroLlantas;
+
+        public SubClaseTerrestre (string NombreTerrestre, int NumeroLlantas)
+        {
+            this.NombreTerrestre = NombreTerrestre;
+            this.NumeroLlantas = NumeroLlantas;
+        }
+        public string getNombreTerrestre()
+        {
+            return NombreTerrestre;
+        }
+        public void setNombreTerrestre(string NombreTerrestre)
+        {
+            this.NombreTerrestre = NombreTerrestre;
+        }
+    }
+}
---
+++ Diagrama/SuperClaseVehiculo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagrama
+{
+    internal class SuperClaseVehiculo
+    {
+        private string NombreVehiculo;
+        private string TipoMotor;
+        private int NumeroPersonas;
+
+
+        public SuperClaseVehiculo(string NombreVehiculo, string TipoMotor, int NumeroPersonas)
+        {
+            this.NombreVehiculo = NombreVehiculo;
+            this.TipoMotor = TipoMotor;
+            this.NumeroPersonas = NumeroPersonas;
+        }
+        public string getNombreVehiculo()
+        {
+            return NombreVehiculo;
+        }
+        public void setNombreVehiculo(string NombreVehiculo)
+        {
+            this.NombreVehiculo = NombreVehiculo;
+        }
+        public string getTipoMotor()
+        {
+            return TipoMotor;
+        }
+        public void setTipoMotor (string TipoMotor )
+        {
+            this.TipoMotor = TipoMotor;
+        }
+
+        public int getNumeroPersonas(int NumeroPersonas)
+        {
+            return NumeroPersonas;
+        }
+        public void setNumeroPersonas(int NumeroPersonas)
+        {
+            this.NumeroPersonas = NumeroPersonas;
+        }
+    }
+}
